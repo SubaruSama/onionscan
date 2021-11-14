298a1c3b41782fa112629b4603cdac44b9c17063
SubaruSama <andresfauque@gmail.com>
2021-11-13 21:21:55 -0300

    Modifying IsOnion function
    Renamed IsOnion to IsOnion_v2 for the onion addresses that is on v2.
    Created the function IsOnion_v3 that checks if the address has length of 56, described at the line 19.

--- utils/validation.go
+++ utils/validation.go
@@ -5,10 +5,18 @@ import (
 	"strings"
 )
 
-func IsOnion(identifier string) bool {
+func IsOnion_v2(identifier string) bool {
 	// TODO: At some point we will want to support i2p
 	if len(identifier) >= 22 && strings.HasSuffix(identifier, ".onion") {
 		matched, _ := regexp.MatchString(`(^|\.)[a-z2-7]{16}\.onion$`, identifier)
+		return matched
+	}
+	return false
+}
+
+func IsOnion_v3(identifer string) bool {
+	if len(identifier) >= 56 && strings.HasSuffix(identifier, ".onion") {
+		matched, _ := regexp.MatchString(`(^|\.)[a-z2-7]{56}\.onion$`, identifier)
 		return matched
 	}
 	return false
